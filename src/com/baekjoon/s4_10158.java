edc3d4feab2aa72d91dc1246ecde32154fd9e277
허상 <daijang35@naver.com>
2022-02-21 21:27:52 +0900

    10158

--- src/com/baekjoon/s4_10158.java
+++ src/com/baekjoon/s4_10158.java
@@ -3,9 +3,6 @@ package com.baekjoon;
 import java.util.Scanner;
 
 public class s4_10158 {
-    static int[] dx={1,1,-1,-1};
-    static int[] dy={1,-1,1,-1};
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
@@ -15,7 +12,6 @@ public class s4_10158 {
         int t = sc.nextInt();
         int x = (q+t)%(2*w);
         int y = (p+t)%(2*h);
-
         x = w - Math.abs(w-x);
         y = h - Math.abs(h-y);
 
